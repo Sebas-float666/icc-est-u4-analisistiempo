e62681bf6d76d1a30f85403eece0a0e075753700
Gabriel Cuenca <gcuenca119@gmail.com>
2026-05-04 11:04:15 -0500

    UPD

--- src/App.java
+++ src/App.java
@@ -1,5 +1,43 @@
+import java.util.Arrays;
+import java.util.concurrent.Callable;
+
+import controllers.SortMethods;
+import models.Results;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        int size=200;
+        int[]arreglo=generate(size);
+        int[]arreglo2=arreglo.clone();
+        int[]arreglo3=Arrays.copyOf(arreglo, size);
+
+        SortMethods metodos = new SortMethods();
+
+        Callable<Void> funcionBubble = () -> {
+            metodos.sortBubble(arreglo2);
+            return null;
+        };
+        Results rBubble = new BenchMarking().medirTiempo(funcionBubble, "Burbuja", size);
+        System.out.println(rBubble.getName() + " = "+ rBubble.getTime());
+
+        
+        Callable<Void> funcionBubbleAvan = () -> {
+            metodos.sortBubbleAvan(arreglo3);
+            return null;
+        };
+        Results rBubbleAvan = new BenchMarking().medirTiempo(funcionBubbleAvan, "Burbuja Avanzada", size);
+        System.out.println(rBubbleAvan.getName() + " = "+ rBubbleAvan.getTime());  
+    }
+    
+      
+    
+    public static int[] generate(int size) {
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++) {
+            int numero = (int) (Math.random() * 10000);
+            array[i] = numero;
+        }
+        return array;
     }
 }
---
+++ src/BenchMarking.java
@@ -0,0 +1,20 @@
+import java.util.concurrent.Callable;
+import models.Results;
+
+public class BenchMarking {
+    public Results medirTiempo(Callable<Void> funcion, String name, int sample){
+    
+    
+    try{
+        long inicio = System.nanoTime();
+        funcion.call();
+        long fin = System.nanoTime();
+        double duracion = fin-inicio;
+        Results resultados= new Results(duracion, name, sample);
+        return resultados;
+    }catch(Exception e){
+        throw new RuntimeException("Error en la funcion");
+    }
+    
+    }
+}
